bdc59c1e0dd8b967e8a761dfca87adb45acdbdcb
Navjot Singh Virk <virksaabnavjot@gmail.com>
2017-08-06 19:05:24 +0100

    Doing things form scratch

--- code/FXcode/src/navsingh/org/uk/fxcode/Main.java
+++ code/FXcode/src/navsingh/org/uk/fxcode/Main.java
@@ -8,16 +8,12 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("fxcode.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public static void main(String args[]){
+        launch(args);
     }
 
-
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
     }
 }
