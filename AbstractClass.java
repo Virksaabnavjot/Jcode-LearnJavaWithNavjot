e581b4f67258ca01c5cfd5e794e087c6d10a7fe3
Navjot Singh Virk <virksaabnavjot@gmail.com>
2017-08-10 19:03:35 +0100

    Finished with Abstract Classes - Working example implemented

--- AbstractClass.java
+++ AbstractClass.java
@@ -22,12 +22,17 @@ SOFTWARE.
 */
 
 /*
-*AbstractClass.java
+*AbstractClass.java or TheAbstractClass.java (important)
 *Purpose: To help understand the meaning and benifits of an abstract class in java
+*Note: Please note this file contains 3 classes -
+1- TheAbstractClass (abstract class with 1 abstract and 1 implemented method)
+2- LetsTryExtendingTheAbstractClass (extends our abstract class)
+3- AbstractClass (contains main method)
 *Date:10/August/2017 | 5:13pm / Dublin,Ireland
 */
 
-abstract class AbstractClass{
+/* This is the actual abstract class */
+abstract class TheAbstractClass{
   /*Explanation: Abstract Class in Java (Hindi) https://www.youtube.com/watch?v=bIQMuFPgKpM
   1- Abstract classes are used to declare common characterstics of subclasses
   Example: Person (Abstract Class) -> Student and Teacher (Subclasses), can
@@ -52,34 +57,53 @@ abstract class AbstractClass{
   String name, address, phone;
   int age;
 
-  //abstract method - more Explanation available in AbstractMethod.java class
+  //abstract method - more Explanation available in AbstractMethods.java class
   abstract void showAllDetails();
 
   //implemented methods
   void showNameAndAddress(){
     name = "Navjot";
     address = "Apt. 14 Henrie...., Ireland";
-    System.out.println("Name: " +name+ "\n" + "Address: " +address);
+    System.out.println("Name: " +name+ "\n" + "Address: " +address+ "\n");
   }
-}
-
-class LetsTryExtendingAbstractClass extends AbstractClass{
-
-void showAllDetails(){
-  name = "Navjot";
-  address = "Apt. 14 Henrie...., Ireland";
-  phone = "+35389211....";
-  age = 22;
-  System.out.println("Name: " +name+ "\n" + "Address: " +address+ "\n" + "Phone: "
-  +phone+ "\n" + "Address: " +address);
-}
 
 }
 
-class MakeUse{
+/*This class extends our abstract class - TheAbstractClass */
+class LetsTryExtendingTheAbstractClass extends TheAbstractClass{
+
+  void showAllDetails(){
+    name = "Navjot";
+    address = "Apt. 14 Henrie...., Ireland";
+    phone = "+35389211....";
+    age = 22;
+    System.out.println("Name: " +name+ "\n" + "Address: " +address+ "\n" + "Phone: "
+    +phone+ "\n" + "Address: " +address+ "\n");
+  }
+
+}
+
+/*This class is a normal class with the main method, that will allow us to try out
+everything our within just one file then creating multiple file and getting lost
+look for TheAbstractClass BELOW to see Explanation and how abstract class is done,
+the name is used as such to keep things simple and organised within the project
+example: if you look at the name AbstractClass.java you know we are dealing with
+abstract classes in their*/
+public class AbstractClass{
   public static void main(String[] args) {
-    LetsTryExtendingAbstractClass l = new LetsTryExtendingAbstractClass();
+
+    //Inheritance
+    LetsTryExtendingTheAbstractClass l = new LetsTryExtendingTheAbstractClass();
+    System.out.println("I am printed using Inheritance concept. \n");
     l.showAllDetails();
     l.showNameAndAddress();
+
+    /*referance variable - here we are creating a referance variable of TheAbstractClass
+    using the subclass as in java its not possible to create objects of abstract classes
+    but we can create referance varibales. */
+    TheAbstractClass a = new LetsTryExtendingTheAbstractClass();
+    System.out.println("I am printed using referance variable concept. \n");
+    a.showNameAndAddress();
   }
+
 }
