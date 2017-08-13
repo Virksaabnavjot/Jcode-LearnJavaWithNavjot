1487b621f7a803553a41ff34a19d520821bf52a6
Navjot Singh Virk <virksaabnavjot@gmail.com>
2017-08-13 18:00:35 +0100

    reserved keywords in java

---
+++ CollectionsInJava.java
@@ -0,0 +1,81 @@
+/*
+Copyright (c) 2017-2018 Navjot Singh Virk (Website: https://navsingh.org.uk),
+(Project Git: https://github.com/virksaabnavjot/jcode)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+*CollectionsInJava.java
+*Purpose: Understanding Collections in Java
+Collections in Java are used in almost every application.
+Java Collections Framework is one of the core part of Java programming language
+*Date: 13 August 2017 | 5:49 pm | Dublin,Ireland
+*/
+
+/*Explanation: http://www.journaldev.com/1260/collections-in-java-tutorial
+
+- What is Java Collections Framework?
+- Benefits of Java Collections Framework
+
+- Java Collections Interfaces
+--- Collection Interface
+--- Iterator Interface
+--- Set Interface
+--- List Interface
+--- Dequeue Interface
+--- Map Interface
+--- ListIterator Interface
+--- SortedSet Interface
+--- SortedMap Interface
+
+- Java Collections Classes
+--- HashSet Class
+--- TreeSet Class
+--- ArrayList Class
+--- LinkedList Class
+--- HashMap Class
+--- TreeMap Class
+--- PriorityQueue Class
+
+- Collections class
+
+- Synchronized Wrappers
+
+- Unmodifiable wrappers
+
+- Thread Safe Collections
+
+- Collections API Algorithms
+--- Sorting
+--- Shuffling
+--- Searching
+--- Composition
+--- Min and Max values
+
+- Java 8 Collections API Features
+- Collection classes in a Nutshell
+
+*/
+
+public class CollectionsInJava{
+
+
+
+}
